39846d7faf6e18375a68624ba6788938b246b09f
pdion891 <pdion891@apache.org>
2015-05-31 07:18:09 -0400

    acs-jenkins cookbook creation

---
+++ cookbooks/acs-jenkins/metadata.rb
@@ -0,0 +1,7 @@
+name             'acs-jenkins'
+maintainer       'cloudstack.apache.org'
+maintainer_email 'pdion891@apache.org'
+license          'Apache 2.0'
+description      'Installs/Configures acs-jenkins'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
